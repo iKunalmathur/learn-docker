9dffb68fd4073d10c7a29d9766c8b3f7825e7b00
Kunal mathur <dev.kunalmathur@gmail.com>
2024-01-07 01:13:14 +0530

    Update react app UI

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -5,32 +5,49 @@ import { getEnv } from "./lib/helper";
 function App() {
   const [todos, setTodos] = useState([]);
 
+  const handleCheckboxChange = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const fetchTodos = async () => {
+    try {
+      const todoApi = getEnv("VITE_API_URL") + "/api/todos";
+      const response = await axios.get(todoApi);
+      setTodos(response.data);
+    } catch (error) {
+      console.error("Error fetching todos:", error);
+    }
+  };
+
   useEffect(() => {
     // Fetch todos when the component mounts
-    const fetchTodos = async () => {
-      try {
-        const todoApi = getEnv("VITE_API_URL") + "/api/todos";
-        const response = await axios.get(todoApi);
-        setTodos(response.data);
-      } catch (error) {
-        console.error("Error fetching todos:", error);
-      }
-    };
-
     fetchTodos();
   }, []); // Empty dependency array to run the effect only once when the component mounts
 
   return (
     <div>
-      <h1>Todo App</h1>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            <input type="checkbox" checked={todo.completed} readOnly />
-            {todo.task}
-          </li>
-        ))}
-      </ul>
+      <div className="todo-app-container">
+        <h1>Todo App</h1>
+        <ul className="todo-list">
+          {todos.map((todo) => (
+            <li
+              key={todo.id}
+              className={`todo-item ${todo.completed ? "completed" : ""}`}
+            >
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => handleCheckboxChange(todo.id)}
+              />
+              <span>{todo.task}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
